59eca4d091ddd05ea40aabec0f9a5e370b3ab162
YZiengg <ntyv69@gmail.com>
2024-11-18 08:16:24 +0700

    fe-qlynthat

--- src/components/AdminProduct/AdminProduct.jsx
+++ src/components/AdminProduct/AdminProduct.jsx
@@ -1,22 +1,78 @@
-import React, { useState } from 'react'
-import { WrapperHeader } from './style';
+import React, { useEffect, useState } from 'react'
+import { WrapperHeader, WrapperUploadFile } from './style';
 import { PlusOutlined } from '@ant-design/icons';
 import TableComponent from '../TableComponent/TableComponent';
 import { Form, Modal, Button } from 'antd';
 import InputComponent from '../InputComponent/InputComponent';
+import { getBase64 } from '../../untils';
+import { useMutationHooks } from "../../hooks/useMutationHook";
+import * as ProductService from "../../services/ProductService";
+import * as message from "../../components/Mesage/Message";
 
 const AdminProduct = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [stateProduct, setStateProduct] = useState({
+        name: '',
+        price: '',
+        description: '',
+        rating: '',
+        image: '',
+        type: '',
+        countInStock: ''
+    })
+    const mutation = useMutationHooks(async (data) => {
+        const res = await ProductService.createProduct(data); // Gọi API và chờ kết quả
+        return res; // Trả về kết quả từ server
+    });
 
-    const handleOk = () => {
+    const { data, isLoading, isSuccess, isError } = mutation;
 
-    }
+    useEffect(() => {
+        if (isSuccess && data?.status === 'OK') {
+            message.success()
+            handleCancel()
+        } else if (isError) {
+            message.error()
+        }
+    }, [isSuccess])
+    const [form] = Form.useForm();
     const handleCancel = () => {
-        setIsModalOpen(false);
+        setIsModalOpen(false); // Đóng modal
+        setStateProduct({
+            name: '',
+            price: '',
+            description: '',
+            rating: '',
+            image: '',
+            type: '',
+            countInStock: ''
+        })
+        form.resetFields()
     };
+
     const onFinish = () => {
-        console.log('finish')
+        mutation.mutate(stateProduct)
+        console.log('finish', stateProduct)
     }
+    const handleOnchange = (e) => {
+        setStateProduct({
+            ...stateProduct,
+            [e.target.name]: e.target.value
+        })
+
+    }
+    const handleOnchangeAvatar = async ({ fileList }) => {
+        if (!fileList || fileList.length === 0) return; // Kiểm tra fileList
+        const file = fileList[0]; // Lấy file đầu tiên
+        if (!file.url && !file.preview) {
+            file.preview = await getBase64(file.originFileObj);
+        }
+        setStateProduct({
+            ...stateProduct,
+            image: file.preview
+        }
+        )
+    };
     return (
         <div>
             <WrapperHeader>Quản lý sản phẩm </WrapperHeader>
@@ -49,55 +105,99 @@ const AdminProduct = () => {
             <div style={{ paddingTop: '15px', border: '1px solid #ddd', borderRadius: '6px' }}>
                 <TableComponent />
             </div>
-            <Modal title="Tạo sản phẩm " open={isModalOpen} onCancel={handleCancel} >
+            <Modal title="Tạo sản phẩm "
+                open={isModalOpen}
+                onCancel={handleCancel}
+
+                footer={[
+                    <Button key="cancel" onClick={handleCancel}>
+                        Cancel
+                    </Button>,
+                ]}
+            >
                 <Form
                     name="basic"
-                    labelCol={{ span: 8 }}
-                    wrapperCol={{ span: 16 }}
-                    initialValues={{ remember: true }}
+                    labelCol={{ span: 6 }}
+                    wrapperCol={{ span: 18 }}
                     onFinish={onFinish}
-                    autoComplete="off"
-                    style={{ marginRight: '20px' }}
+                    autoComplete="on"
+                    form={form}
                 >
                     <Form.Item
                         label="Name"
-                        name="Name"
+                        name="name"
                         rules={[{ required: true, message: 'Please input your Name!' }]}
-
+                        labelAlign="left" // Căn lề trái
                     >
-                        <InputComponent />
+                        <InputComponent value={stateProduct.name} onChange={handleOnchange} name="name" />
                     </Form.Item>
-
                     <Form.Item
                         label="Type"
                         name="type"
                         rules={[{ required: true, message: 'Please input your type!' }]}
+                        labelAlign="left" // Căn lề trái
                     >
-                        <InputComponent />
+                        <InputComponent value={stateProduct.type} onChange={handleOnchange} name="type" />
                     </Form.Item>
+
+
                     <Form.Item
                         label="Count InStock"
                         name="countInStock"
                         rules={[{ required: true, message: 'Please input your count InStock!' }]}
+                        labelAlign="left" // Căn lề trái
                     >
-                        <InputComponent />
+                        <InputComponent value={stateProduct.countInStock} onChange={handleOnchange} name="countInStock" />
                     </Form.Item>
                     <Form.Item
                         label="Price"
                         name="price"
                         rules={[{ required: true, message: 'Please input your price!' }]}
+                        labelAlign="left" // Căn lề trái
                     >
-                        <InputComponent />
+                        <InputComponent value={stateProduct.price} onChange={handleOnchange} name="price" />
                     </Form.Item>
                     <Form.Item
                         label="Description"
                         name="description"
                         rules={[{ required: true, message: 'Please input your description!' }]}
+                        labelAlign="left" // Căn lề trái
                     >
-                        <InputComponent />
+                        <InputComponent value={stateProduct.description} onChange={handleOnchange} name="description" />
                     </Form.Item>
-                    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-                        <Button type="primary" htmlType="submit">
+                    <Form.Item
+                        label="Rating"
+                        name="rating"
+                        rules={[{ required: true, message: 'Please input your rating!' }]}
+                        labelAlign="left" // Căn lề trái
+                    >
+                        <InputComponent value={stateProduct.rating} onChange={handleOnchange} name="rating" />
+                    </Form.Item>
+                    <Form.Item
+                        label="Image"
+                        name="image"
+                        rules={[{ required: true, message: 'Please input your Image!' }]}
+                        labelAlign="left" // Căn lề trái
+                    >
+                        <WrapperUploadFile onChange={handleOnchangeAvatar} maxCount={1}>
+                            <Button >Select File</Button>
+                            {stateProduct?.image && (
+                                <img
+                                    src={stateProduct?.image}
+                                    style={{
+                                        height: "60px",
+                                        width: "60px",
+                                        borderRadius: " 50%",
+                                        objectFit: "cover",
+                                        marginLeft: "10px"
+                                    }}
+                                    alt="avatar"
+                                />
+                            )}
+                        </WrapperUploadFile>
+                    </Form.Item>
+                    <Form.Item wrapperCol={{ offset: 6, span: 40 }}>
+                        <Button type="primary" htmlType="submit" style={{ width: '95px' }}>
                             Submit
                         </Button>
                     </Form.Item>
--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,12 +1,23 @@
 import axios from "axios";
 
 export const getAllProduct = async () => {
-    try {
-      const res = await axios.get(
-        `${process.env.REACT_APP_API_URL}/product/get-all`
-      );
-      return res.data;
-    } catch (error) {
-      return error.response.data;
-    }
-  };
+  try {
+    const res = await axios.get(
+      `${process.env.REACT_APP_API_URL}/product/get-all`
+    );
+    return res.data;
+  } catch (error) {
+    return error.response.data;
+  }
+};
+export const createProduct = async (data) => {
+  try {
+    const res = await axios.post(
+      `${process.env.REACT_APP_API_URL}/product/create`,
+      data // Truyền toàn bộ đối tượng data
+    );
+    return res.data;
+  } catch (error) {
+    return error.response.data;
+  }
+};
